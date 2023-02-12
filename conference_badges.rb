1813f030112642ef064c5e3f530dd8ff546ca6f4
Samueelx <gsammygitau@gmail.com>
2023-02-12 07:24:50 +0300

    done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}.";
+end
+
+def batch_badge_creator(names)
+    messages = names.collect {|name| "Hello, my name is #{name}."}
+    return messages;
+end
+
+def assign_rooms(speakers)
+    messages = speakers.map.with_index do |speaker, index|
+        "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+    end
+    return messages;
+end
+
+def printer(speakers)
+    greetings = batch_badge_creator(speakers);
+    greetings.each {|greeting| puts greeting};
+
+    rooms = assign_rooms(speakers);
+    rooms.each {|room| puts room}
+end
